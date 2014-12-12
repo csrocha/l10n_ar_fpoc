5466da608e9ed12ea907463bf99179c80dba49f1
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-12-12 18:32:29 -0300

    [FIX] Rename fpoc.user from fiscal_printer.user

--- journal.py
+++ journal.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    fiscal_printer
-#    Copyright (C) 2014 No author.
-#    No email
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 from openerp import netsvc
 from openerp.osv import osv, fields
@@ -47,7 +27,7 @@ class account_journal(osv.osv):
 
     _name = "account.journal"
 
-    _inherit = ["account.journal", "fiscal_printer.user"]
+    _inherit = ["account.journal", "fpoc.user"]
 
     _columns = {
         'use_fiscal_printer': fields.boolean('Associated to a fiscal printer'),
