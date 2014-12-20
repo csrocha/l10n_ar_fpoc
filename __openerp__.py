2b6329112206df2e875af5d93fe5bf25b7583934
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2014-12-20 18:54:01 -0300

    [FIX] Change description and name of the addon

--- __openerp__.py
+++ __openerp__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-{   'active': False,
+{
+    'active': False,
     'author': 'Moldeo Interactive',
     'category': 'base.module_category_hidden',
     'demo_xml': [],
     'depends': ['account', 'fpoc', 'l10n_ar_invoice'],
-    'description': 'Fiscal Printer on the Cloud module for Argentina Localization',
+    'name': 'Fiscal Printer on the Cloud support for Argentina Localization',
+    'description': '___',
     'init_xml': [],
     'installable': True,
     'license': 'AGPL-3',
-    'name': 'l10n_ar_fpoc',
     'test': [ ],
     'data': [
         'data/journal_view.xml',
@@ -19,6 +20,6 @@
     'css': [],
     'qweb': [],
     'version': '0.1',
-    'website': ''}
-
+    'website': ''
+}
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
